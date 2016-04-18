e2b1939bd4e7b2e298e9695e4dffde92d770d111
Sergio <sergiobmaciel@gmail.com>
2016-04-18 00:17:14 -0300

    ok

--- src/br/com/maciel/controle_casa_v2/utils/ClienteHttpGet.java
+++ src/br/com/maciel/controle_casa_v2/utils/ClienteHttpGet.java
@@ -1,40 +1,58 @@
 package br.com.maciel.controle_casa_v2.utils;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpGet;
+//import org.apache.http.impl.client.DefaultHttpClient;
 
 public class ClienteHttpGet implements Runnable {
-	
-private String url="";
-    
+
+    private URL url = null;
+
     /*
      * Proprio contrutor ja requisita a url e inicia a Thread;
      */
     public ClienteHttpGet(String urlParam) {
         super();
-        url=urlParam;
+        url = new URL(urlParam);
         //System.out.println("url="+url);
         Thread t = new Thread(this);
         t.start();
     }
-    
+
     public void fim() {
-     
+
     }
-    
-	@Override
-	public void run() {
-        HttpClient cliente = new DefaultHttpClient();
+
+    @Override
+    public void run() {
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+
+           /* HttpClient cliente = new DefaultHttpClient();
         HttpGet requiscao = new HttpGet();
         try {
             requiscao.setURI(new URI(url));
             cliente.execute(requiscao);
         } catch (Exception e) {
             e.printStackTrace();
+        }*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
         }
-	}
 
+    }
 }
